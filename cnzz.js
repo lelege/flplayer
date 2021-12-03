eb9f1401233f19050b9d6044569d5a080cc0b867
乐乐 <35110476+lelege@users.noreply.github.com>
2021-12-03 22:35:17 +0800

    Create cnzz.js

---
+++ cnzz.js
@@ -0,0 +1,9 @@
+(function() {
+	var cnzz_s_tag = document.createElement('script');
+	cnzz_s_tag.type = 'text/javascript';
+	cnzz_s_tag.async = true;
+	cnzz_s_tag.charset = "UTF-8";
+	cnzz_s_tag.src = "https://v1.cnzz.com/z_stat.php?id=1280622668&web_id=1280622668";
+	var root_s = document.getElementsByTagName('script')[0];
+	root_s.parentNode.insertBefore(cnzz_s_tag, root_s)
+})();
